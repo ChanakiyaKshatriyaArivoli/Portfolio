155f002043f4a30b522af82e1ce265a28c6418a5
HEMANTH M <128402149+hemanth0525@users.noreply.github.com>
2024-11-27 20:54:43 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/CKHeroSquare.JPG";
+import profilePic from "../assets/CKHeroSquare.jpg";
 import { motion } from "framer-motion";
 
 const container = (delay) => ({
